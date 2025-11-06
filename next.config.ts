b91e828d43b42455f1ff52aa974f77d1b39066b9
andhikaeffendy14 <andhika.effendy@bankraya.co.id>
2025-11-06 13:02:48 +0700

    add: rebase

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
